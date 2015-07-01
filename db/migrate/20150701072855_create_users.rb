74435cdb80b6d6cc52d1c6e9167afebd0a00fc07
WeijiaXu <weijia.xu.ixia@gmail.com>
2015-07-01 22:03:28 +0800

    update table users

--- db/migrate/20150701072855_create_users.rb
+++ db/migrate/20150701072855_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      	t.string :name
+	t.string :password
+	t.date :register_date
+	t.integer :root_id
       t.timestamps null: false
     end
   end
