3a291788d148b55e5aed9abbc06fe937bcdafe54
srzmldl <ldlong@mail.ustc.edu.cn>
2015-07-03 00:20:06 +0800

    complete create in users_controller.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,9 +4,18 @@ class Api::V1::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  #def create
-   # @user = User.find_by(name: create_params[:name])
-   # if (@user)
-   # end
-    
+  def create
+    @user = User.find_by(name: create_params[:name])
+    @state = 0;
+    # -1 for exist
+    if @user
+      @state = -1
+    else
+      @state = 0
+      @user = register(name: create_params[:name], psw: create_params[:password]);
+    end
+
+    def create_params
+      params.require(:user).permit(:name, :password)
+    end
 end
