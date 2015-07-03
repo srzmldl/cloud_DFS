f814ec0133320f3cd7056b55290a84e85a758382
srzmldl <ldlong@mail.ustc.edu.cn>
2015-07-04 00:17:26 +0800

    conflict fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,16 +14,6 @@ class User < ActiveRecord::Base
 	#|psw|  < 6 return -2
 	# user exist return -3
 	def register(name = '', psw = '')
-<<<<<<< HEAD
-      if name.length < 3
-        return -1
-      elsif	psw.length < 6
-        return -2
-      else 
-        user = User.create(name: name, password: psw, register_date: Time.now)
-        return user
-      end
-=======
 		if name.length < 3
 			return -1
 		elsif	psw.length < 6
@@ -36,7 +26,6 @@ class User < ActiveRecord::Base
 			user = User.create(name: name, password: psw, register_date: Time.now)
 			return user
 		end
->>>>>>> 65d261dd1b26c1e72c399beb0f875424b8412ee1
 	end
 	#root: root directory file (virtual)
 	#this method is used to modify the root_id
@@ -44,20 +33,20 @@ class User < ActiveRecord::Base
 	def redirect(name = '', root = '')
 		user = User.find_by(name: name)
 		if user == nil
-          return false
+			return false
 		else 
-          file = Virfile.new
-          file = file.find_by_path(root)
-          if file	== nil or file.fa != 0 or file.visible == false
-            return false
-          else
-            user.root_id = file.id
-            user.save
-            return true
-          end
+			file = Virfile.new
+			file = file.find_by_path(root)
+			if file	== nil or file.fa != 0 or file.visible == false
+				return false
+			else
+				user.root_id = file.id
+				user.save
+				return true
+			end
 		end
 	end
-    
+
 	def newpsw(name = '', psw = '')
 		if psw.length < 6
 			return false
