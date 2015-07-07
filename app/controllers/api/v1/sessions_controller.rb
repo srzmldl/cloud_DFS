08e697bb89e20c1f6d99e1cd7568de25de54d1e3
srzmldl <ldlong@mail.ustc.edu.cn>
2015-07-07 21:33:53 +0800

    finish login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,17 +3,18 @@ class Api::V1::SessionsController < ApplicationController
 
   skip_before_filter  :verify_authenticity_token
   def create
-    @flag = login(name: create_params[:name], password: create_params[:password])
+    @flag = User.login(create_params[:name], create_params[:password])
     if @flag
       @user = User.find_by(name: create_params[:name])
     else
       @user = nil
     end
-
-    private
-    
-    def create_params
-      params.require(:user).permit(:name, :password)
-    end
   end
+  
+  private
+  
+  def create_params
+    params.require(:user).permit(:name, :password)
+  end
+  
 end
--- app/views/api/v1/sessions/create.json.jbuilder
+++ app/views/api/v1/sessions/create.json.jbuilder
@@ -1,3 +1,6 @@
-json.session do
-  json.(@user, :id, :name, :register_date, :root_id， @flag)
-endc
+json.user do
+  if (@user != nil)
+    json.(@user, :id, :name, :register_date, :root_id)
+  end
+  json.flag(@flag)
+end
